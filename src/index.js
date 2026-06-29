96d1d192d667174087ea50f10fac6eaf8fbbb9b7
Daria Czupreta <daria.czupreta@gmail.com>
2026-06-29 14:43:27 +0200

    index.js added two domens allowed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 export default {
   async fetch(request) {
+    const origin = request.headers.get("Origin");
+
+    const allowedOrigins = [
+      "http://localhost:3000",
+      "https://currency-api.daria-czupreta.workers.dev", // <-- zamień na swoją domenę produkcyjną
+    ];
+
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": allowedOrigins.includes(origin)
+        ? origin
+        : "https://currency-api.daria-czupreta.workers.dev", // <-- zamień na swoją domenę
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    };
+
+    // Obsługa preflight
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 204,
+        headers: corsHeaders,
+      });
+    }
 
     const country = request.cf?.country;
     const continent = request.cf?.continent;
@@ -12,9 +34,11 @@ export default {
       currency = "EUR";
     }
 
-    return Response.json({
-      currency
-    });
-
-  }
-}
+    return Response.json(
+      { currency },
+      {
+        headers: corsHeaders,
+      }
+    );
+  },
+};
